1ea9961aa8bda84d95108a5ddfdd1617293cc062
kurtis <kurtis@us.ibm.com>
2025-05-29 16:48:32 -0500

    fms/model_load.py be sure to remove later

--- fms/model_load.py
+++ fms/model_load.py
@@ -3,12 +3,12 @@ from fms.models import get_model
 from fms.utils import generation, tokenizers
 from fms.utils.generation import generate, pad_input_ids
 import pprint
-from pycony import * 
+# from pycony import * 
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-MODEL_PATH="/mnt/aiu-models-en-shared/models/hf/qwen/qwen3-1.7B"
+MODEL_PATH="/home/kurtis/tmp/models/qwen3-1.7B-old"
 
 model = get_model(
     architecture='hf_pretrained',
@@ -23,3 +23,4 @@ model = get_model(
     linear_config={'linear_type': 'torch_linear'},
     fused_weights=False,
 )
+
